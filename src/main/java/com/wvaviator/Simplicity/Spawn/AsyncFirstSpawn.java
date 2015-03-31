07f26ca4f7d11ab36e4ecbcb5a9482edf828f4d9
WVAviator <wvaviator@gmail.com>
2015-03-31 16:02:48 -0400

    Removed some logging

--- src/main/java/com/wvaviator/Simplicity/Spawn/AsyncFirstSpawn.java
+++ src/main/java/com/wvaviator/Simplicity/Spawn/AsyncFirstSpawn.java
@@ -15,14 +15,12 @@ public class AsyncFirstSpawn extends Thread{
 	
 	public void run(){
 		
-		System.out.println("Executing async task...");
 		try {
 			this.sleep(100);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Async task complete");
 		
 		player.setPositionAndUpdate(pos.getX() + 0.50, pos.getY(), pos.getZ() + 0.50);
 		
